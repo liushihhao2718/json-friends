efc6f7daab8eb8b819217a872b61006640b6d7cd
Tom1_Liu <tom1_liu@asus.com>
2024-03-16 16:01:20 +0800

    use vue router

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,12 @@
           @expand="siderOpen = true"
         >
           <n-menu
-            :value="current_view"
             :collapsed-width="64"
             :collapsed-icon-size="22"
             :options="menuOptions"
-            @update:value="handleApp"
           />
         </n-layout-sider>
-        <component :is="() => main_view_render()" />
+        <router-view></router-view>
       </n-layout>
     </n-flex>
 
@@ -30,50 +28,32 @@
 </template>
 
 <script setup lang="tsx">
-import JsonTable from "./components/JsonTable.vue";
-import JsonConvertor from "./components/JsonConvertor.vue";
 import "splitpanes/dist/splitpanes.css";
 import window_grid from "./assets/icon/window-grid.svg";
 import { NFlex, NLayout, NLayoutSider, NIcon, NMenu } from "naive-ui";
-
+import { RouterView, RouterLink } from 'vue-router';
 import { h, ref } from "vue";
 import { PlayForward } from "@vicons/ionicons5";
 
-function renderIcon(icon) {
+function renderIcon(icon: any) {
   return () => h(NIcon, null, { default: () => h(icon) });
 }
 
 const menuOptions = [
   {
-    label: "Json to Table",
+    label: ()=> <RouterLink to={{path:"/json2table"}}/>,
     key: "json2table",
     icon: renderIcon(<img src={window_grid} style="width: 100%;"></img>),
   },
   {
-    label: "Json Convert",
+    label: ()=> <RouterLink to={{path:"/jsonConvertor"}}/>,
     key: "jsonConvertor",
     icon: renderIcon(PlayForward),
   },
 ];
 
-const current_view = ref("jsonConvertor");
-
 const siderOpen = ref(false);
 
-function handleApp(key: string, item: string) {
-  console.log(key);
-
-  current_view.value = key;
-}
-
-function main_view_render(): JSX.Element {
-  const main_view = {
-    json2table: <JsonTable></JsonTable>,
-    jsonConvertor: <JsonConvertor></JsonConvertor>,
-  };
-
-  return main_view[current_view.value];
-}
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import * as VueRouter from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: "/",
+    redirect: "/json2table",
+  },
+  {
+    name:"json2table",
+    path: "/json2table",
+    component: ()=>import("./components/JsonTable.vue"),
+  },
+  {
+    name:"jsonConvertor",
+    path: "/jsonConvertor",
+    component: ()=>import("./components/JsonConvertor.vue"),
+  },
+];
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory("/json-friends/"),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
